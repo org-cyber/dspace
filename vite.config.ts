8de191c8ee96ae0a0413714ac02dcc1229c29542
Pixelpro <pixelpro548@gmail.com>
2025-12-13 15:40:48 +0100

    fixed the page breaking during build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
   ],
   server: {
     host: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'main.html'),
+      },
+    },
   },
 });
